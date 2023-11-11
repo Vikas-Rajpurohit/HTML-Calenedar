cc8ed9dc2d9039d1915ba8acd8ae070cac01f3c9
Vikas-Rajpurohit <vikas.rajpurohit@spit.ac.in>
2023-11-11 22:53:38 +0530

    All Calendars

---
+++ events.js
@@ -0,0 +1,26 @@
+var events = [
+    {
+      id: "1",
+      name: "test1",
+      start: 1,
+      month: 6,
+      end: 3,
+    },
+    {
+      id: "2",
+      name: "test2",
+      start: 2,
+      month: 6,
+      end: 3,
+    },
+    {
+      id: "3",
+      name: "test3",
+      start: 29,
+      month: 6,
+      end: 6,
+    },
+    // Add more objects as needed
+  ];
+
+export default events;
